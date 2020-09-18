e9c2d65dad878f095936246b861a7493b57ccc32
takara-sumimoto <96.sumimoto@gmail.com>
2020-09-18 16:53:06 +0900

    s

--- resources/views/top/welcome.blade.php
+++ resources/views/top/welcome.blade.php
@@ -10,7 +10,7 @@
   <link
     href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
     rel="stylesheet" />
-<link href="https://fonts.googleapis.com/css2?family=Kite+One&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Kite+One&display=swap" rel="stylesheet">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('css/top/welcome.css') }}">
   <title>RecipeManager</title>
@@ -84,6 +84,9 @@
         logout() {
         this.$refs.logout.submit();
         },
+      },
+      mounted() {
+        alert("メールアドレス:owner-a@test.com  パスワード:ownerowner  でログインしていただくと、シーティングされたお店のページにアクセスできます")
       },
     });
   </script>
